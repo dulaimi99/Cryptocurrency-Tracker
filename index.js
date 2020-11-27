3b8a2b6a848ac0f9270ba7f9f61a8118d92a9ed5
Omar Scott <vscott@pdx.edu>
2020-11-27 10:41:55 -0800

    please look at cors error

---
+++ index.js
@@ -0,0 +1,9 @@
+function getnews(){
+    fetch('https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=bitcoin&apiKey=1f0af31cdfd047d4b02f887df2b38601', {headers: new Headers({"X-Requested-With":"cryto"})})
+    .then(a => a.json())
+    .then(response => {
+        for (var i =0; i < response.articles.length; i++){
+            document.getElementById("output").innerHTML+="<div style='padding-top: 20px;'><img style='float:left; width: 100px;' src='"+response.articles[i].urlToImage+"'><h4>"+response.articles[i].title+"</h4>"+response.articles[i].source.name+"<br>"+response.articles[i].description+" <a href='"+response.articles[i].url+"'target='_blank'>"+response.articles[i].url+"</a></div>";
+        }
+    })
+}
