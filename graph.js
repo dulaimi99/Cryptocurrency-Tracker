4c159ee81eb1961285a3c62fa6f4bd6d653e6ef1
Noah Hill <nohil2@pdx.edu>
2020-11-27 19:28:50 -0800

    Full graph implementation
    Kept moment.js only for formating purposes; otherwise not needed.

--- graph.js
+++ graph.js
@@ -1,6 +1,3 @@
-
-console.log(moment())
-
 charting()
 
 function charting(){
@@ -13,60 +10,48 @@ function charting(){
         var dates = [];
         for(var i = 0; i < temp_prices.length; i++) {
             prices.push(temp_prices[i][1]);
-            let temp = new Date(temp_prices[i][0])
-            dates.push(temp);
+            dates.push(moment(temp_prices[i][0]).format('lll'));
         }
 
         for(i = 0; i < 8; i++) {
-            console.log(dates[i].toDateString())
+            console.log(dates[i])
         }
 
         var m_cap = data.market_caps;
         var t_vol = data.total_volumes;
 
-        //console.log(prices, m_cap, t_vol);
+
         var ctx = document.getElementById('chart').getContext('2d');
         var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: dates,
             datasets: [{
-                label: 'Daily Prices',
+                label: 'Daily Price',
                 data: prices,
-    /*             backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                backgroundColor: [
+                    'rgba(99, 255, 132, 0.2)',
+
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(99, 255, 132, 1)',
+
                 ],
-                borderWidth: 1 */
+                borderWidth: 1 
             }]
             },
             options: {
                 responsive: true,
                 maintainAspectRatio: false,
+                ticks: {
+                    source: 'labels'
+                },
                 scales: {
-                    xAxes: [{
-                        type: 'time',
-                        time: {
-                            displayFormats: {
-                                day: 'MMM D'
-                            }
-                        }
-                    }],
                     yAxes: [{
                         ticks: {
-                            beginAtZero: true
+                            callback: function(value, index, values) {
+                                return '$' + value;
+                            }
                         }
                     }]
                 }
