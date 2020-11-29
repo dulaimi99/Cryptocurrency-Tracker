61db61b22bee8b9cad3eddf02a856bcfbaaee17b
Ahmed <ahmeddulaimi99@gmail.com>
2020-11-29 02:21:04 -0800

    Changes to index.html

--- test.js
+++ test.js
@@ -1,55 +1,11 @@
 const { get } = require("http");
 var fetch = require("node-fetch");
-//const { resolve } = require("path");
 
-/*
-const key = 'dddd7b23-d72f-48c8-934f-33136b9b827b'
-let init = 
-{
-    method: 'GET',
-    Headers: {
-        'X-CMC_PRO_API_KEY': key 
-    },
-    mode: 'cors',
-
-}
-*/
-
-
-
-
-
-
-
-
-
-/*let init = 
-{
-    method: 'GET',
-    mode: 'cors',
-    headers:
-    {
-        
-        //host : 'cors-anywhere.herokuapp.com',
-    }
-}
-*/
-/*
-
-queryparams: {
-        CMC_PRO_API_KEY: process.env.CMC_API_KEY,
-      },
-
-
-headers: {
-        'X-CMC_PRO_API_KEY': 'dddd7b23-d72f-48c8-934f-33136b9b827b',
-      },
-
-*/
 
 let cors= 'https://cors-anywhere.herokuapp.com/'
 let api_link = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=25"
 let url = cors + api_link;
+
 
 let headers = {
     'Accept': '*/*',
@@ -64,7 +20,6 @@ let headers = {
     'Sec-Fetch-Site': 'cross-site',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
     'X-CMC_PRO_API_KEY': 'dddd7b23-d72f-48c8-934f-33136b9b827b',
-
 }
 
 let query_param = {
@@ -95,13 +50,8 @@ fetch(url,{headers,query_param})
 
 function do_things(obj)
 {
-    console.log(obj.data[0].quote)
+    console.log(obj.data)
     console.log(url)
-
-    
-    //console.log(obj.data.length)
-    //top_25 = obj.data.slice[0,25]
-    //console.log(top_25)
 }
 
 function resolve_issue(obj)
