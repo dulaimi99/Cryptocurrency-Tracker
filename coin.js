096aac075ab51aa8195b401daedd10bf35ba9cea
Omar Scott <vscott@pdx.edu>
2020-11-28 23:10:30 -0800

    update table info

--- coin.js
+++ coin.js
@@ -16,6 +16,27 @@ var myArray = []
 			buildTable(myArray)
 			console.log(myArray)
 		}
+    })
+    
+    $('th').on('click', function(){
+		var column = $(this).data('column')
+		var order = $(this).data('order')
+		var text = $(this).html()
+		text = text.substring(0, text.length - 1)
+
+		if(order == 'desc'){
+			$(this).data('order', "asc")
+			myArray = myArray.sort((a,b) => a[column] > b[column] ? 1 : -1)
+			text += '&#9660'
+
+		}else{
+			$(this).data('order', "desc")
+			myArray = myArray.sort((a,b) => a[column] < b[column] ? 1 : -1)
+			text += '&#9650'
+
+		}
+		$(this).html(text)
+		buildTable(myArray)
 	})
 
     $('#search_input').on('keyup', function(){
@@ -44,8 +65,8 @@ var myArray = []
 	function buildTable(data){
 		var table = document.getElementById('myTable')
         table.innerHTML = ''
-		// for (var i = 0; i < data.length; i++){
-        for (var i = 1; i <= 25; i++){
+		for (var i = 0; i < data.length; i++){
+        //for (var i = 1; i <= 25; i++){
             var row = `<tr>
                             <td>${i}</td>
 							<td>${data[i].name} (${data[i].symbol})</td>
