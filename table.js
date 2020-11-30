f9a0d28694014b79986d0fa302c7c209976ac653
Ahmed <ahmeddulaimi99@gmail.com>
2020-11-29 16:38:43 -0800

    Updated table.js, and removed redundancies

--- table.js
+++ table.js
@@ -1,7 +1,9 @@
 /**************************************
  Populuating cryptocurrency table
  *************************************/
-function request_table()
+
+//Request data from CMC API
+ function request_table()
 {
     //using cors anywhere to get around cors issue
     let cors= 'https://cors-anywhere.herokuapp.com/'
@@ -38,7 +40,7 @@ function request_table()
         )
         .then(response => response.json())
         .then(json => add_listings(json.data))
-        .catch(err => resolve_issue(err))
+        .catch(err => console.log(err))
 }
 
 //populate table with entries
@@ -52,6 +54,7 @@ function add_listings(data)
     var i;
     const list_length = data.length
 
+    //add listings to table
     for(i = 0; i < list_length; i++)
     {
     var row = 
@@ -67,58 +70,27 @@ function add_listings(data)
     table.innerHTML += row
     
     }
-
-}
-
-//function to catch error
-function resolve_issue(obj)
-{
-    console.log(obj)
 }
 
 
 /******************************
- Table ordering & Search bar 
+ Search
  *****************************/
+/*
+function load_crypto()
+{
+    //search input element
+    search_value = document.getElementById("search_content").value;
 
-$('th').on('click', function(){
-    var column = $(this).data('column')
-    var order = $(this).data('order')
-    var text = $(this).html()
-    text = text.substring(0, text.length - 1)
-  
-    if(order == 'desc'){
-      $(this).data('order', "asc")
-      myArray = myArray.sort((a,b) => a[column] > b[column] ? 1 : -1)
-      text += '&#9660'
-  
-    }else{
-      $(this).data('order', "desc")
-      myArray = myArray.sort((a,b) => a[column] < b[column] ? 1 : -1)
-      text += '&#9650'
-  
-    }
-    $(this).html(text)
-    buildTable(myArray)
-  })
-  
-  $('#search_input').on('keyup', function(){
-      var value = $(this).val()
-      console.log('Value:', value)
-      var data = searchTable(value, myArray)
-      buildTable(data)
-  })
-  
-  function searchTable(value, data){
-      var filteredData = []
-  
-      for(var i = 0; i < data.length; i++){
-          value = value.toLowerCase()
-          var name = data[i].name.toLowerCase()
-  
-          if(name.includes(value)){
-              filteredData.push(data[i])
-          }
-      }
-      return filteredData
-  }
+    //log search
+    console.log(search_value)
+
+    //load new page
+    window.location.("details.html")
+}
+*/
+
+
+
+
+
